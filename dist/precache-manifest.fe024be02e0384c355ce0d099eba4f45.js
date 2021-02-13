8af9a34fd8eaccba70ee156d5c31476a05ba02bd
Saugandh Karan <saugandhkaran@gmail.com>
2021-02-13 14:20:26 +0100

    adding dist subtree

---
+++ dist/precache-manifest.fe024be02e0384c355ce0d099eba4f45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "730e1767ae38a7fbd9e0",
+    "url": "/medicalapp/css/about.9ad4edf8.css"
+  },
+  {
+    "revision": "e63d17fe7218571177ff",
+    "url": "/medicalapp/css/app.3f4dea39.css"
+  },
+  {
+    "revision": "985f9159940a56aeeaf6be91537e231f",
+    "url": "/medicalapp/index.html"
+  },
+  {
+    "revision": "730e1767ae38a7fbd9e0",
+    "url": "/medicalapp/js/about.a2eb669d.js"
+  },
+  {
+    "revision": "e63d17fe7218571177ff",
+    "url": "/medicalapp/js/app.c0705be2.js"
+  },
+  {
+    "revision": "dcdf5be343b133eaabed",
+    "url": "/medicalapp/js/chunk-vendors.78b60929.js"
+  },
+  {
+    "revision": "edd9c2ae76874385fb889a5a8e630f12",
+    "url": "/medicalapp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/medicalapp/robots.txt"
+  }
+]);
